b45f8e8ad733f2389080c39d78137c3fa843c3e5
Rashad Muntar <muntartoure@gmail.com>
2022-09-06 20:11:14 +0000

    implemented Input component

---
+++ src/components/Input/Input.types.ts
@@ -0,0 +1,12 @@
+import { ChangeEventHandler } from "react"
+
+export interface InputProps {
+    label?:string
+    success?:boolean
+    id?:string
+    placeholder?:string
+    disable?:boolean
+    message?:string
+    error?:string
+    onChange?: ChangeEventHandler
+}
