692ac9ff3846f3805c37165f1f65dbc4b3fe5da6
Rashad Muntar <muntartoure@gmail.com>
2022-09-10 22:24:46 +0000

    add wrapper ui

---
+++ src/components/Wrapper/Wraper.types.ts
@@ -0,0 +1,8 @@
+interface WrapperProps {
+    maxW: 'sm' | 'large' | 'xlarge'
+    border?: string
+    borderR?: 'sm' | 'large' | 'xlarge'
+    overflow?: 'hidden' | "show"
+}
+
+export default WrapperProps
